5dda9ea560dcb1376b278cb4a8aad29583394b30
HaxagonusD <jquezad1@binghamton.edu>
2020-10-10 18:43:42 -0400

    Done

--- encryptPhone.js
+++ encryptPhone.js
@@ -3,6 +3,13 @@
  * @param number will always be a string formatted as such '111-223-1123'. Your function should return the first 6 digits as * and only the last 4 shown. Ex:
  * ' 111-223-1123 ' should be ' ***-***-1123 '
  */
+const number = "888-222-3333";
 
+const hashing = (something)=>
+  something
+  .split("")
+  .reduce((acc, curr,index, array)=>{
+    return index <= 6 && curr !== "-"? acc + "*" : acc + curr; 
+  },"")
 
 module.exports = encryptPhoneNumber;
